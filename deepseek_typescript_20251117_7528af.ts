c332c2467ada4952bb6c6182cfecd10b4142c7ad
somdateladok7-commits <somdateladok7@gmail.com>
2025-11-16 23:18:40 -0300

    Add files via upload

---
+++ deepseek_typescript_20251117_7528af.ts
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Painel from './components/Painel';
+import Reputacao from './components/Reputacao';
+import Sobre from './components/Sobre';
+// ... outros imports
